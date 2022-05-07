dd7f7393622a2c68d1dfa31047161b58f8d689ae
Lucas LOUIS <lucas.louis@epitech.eu>
2022-05-07 15:21:30 +0200

    fix: add CORS fix

--- back/main.py
+++ back/main.py
@@ -2,8 +2,19 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from routes.form import router as formRouter
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*", "http://localhost:8000/"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/static", StaticFiles(directory="../front/build/static"))
 app.include_router(formRouter, prefix="/api")
