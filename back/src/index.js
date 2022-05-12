e8b474b3454c7fdddcec44a78dc06a3f0db15b73
Amoz Pay <amoz.pay@epitech.eu>
2022-05-12 21:58:19 +0200

    removed unused back functionnalities

--- back/src/index.js
+++ back/src/index.js
@@ -1,12 +1,10 @@
 const express =  require('express');
-const router =  require('./routes');
 const config =  require('./config');
 const app = express();
 const path = require('path');
 const cors = require('cors')
 
 app.use(cors())
-app.use('/api', router)
 app.use('/static', express.static(path.resolve('../front/build/static')))
 
 app.get('/', (req, res) => {
--- back/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const controllers = require('../controllers');
-const router = express.Router();
-
-router.post('/identity', controllers.postIdentity);
-router.get('/identity/:id', controllers.getIdInfo);
-router.get('/tokens/:wallet_address', controllers.getAllTokens);
-
-module.exports = router;
