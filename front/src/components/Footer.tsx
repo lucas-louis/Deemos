9270829c512c6652a7848a3851b3d6ca5d3be31b
Amoz Pay <amoz.pay@epitech.eu>
2022-05-13 11:58:00 +0200

    style: underline developpers section

--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = (): JSX.Element => (
 	<Box zIndex={100} height="40px !important" minH="40px !important">
 		<Center as="footer" w="100vw" h="40px" bg="rgba(0, 0, 255, 0.1)" position="fixed" left="0" bottom="0">
 			<Text color="white">
-				Made with 💜 by PoC - <Link href="https://github.com/AmozPay/deemos-js">Developers</Link>
+				Made with 💜 by PoC - <Link href="https://github.com/AmozPay/deemos-js"><u>Developers</u></Link>
 			</Text>
 		</Center>
 	</Box>
